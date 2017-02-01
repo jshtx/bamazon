cf42a06b3bcf59f5b3c9c01a4aa091739da7acde
jshtx <shuajo83@gmail.com>
2017-01-31 22:36:29 -0600

    cleaned up the code a bit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
+//npm packages that are required
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
 
 
-
+//define the var connection so it can create a connection to the bamazon database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -16,13 +17,16 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+//start connection to bamazon database
 connection.connect(function(err) {
   if (err) throw err;
-
+  //initial build of the table
    buildTable();
    
 });
 
+//start of reusable functions
+//==========================
 function buildTable() {
 
 	var table = new Table({ head: ["Product ID", "Product", "Department", "Price", "Quantity"] });
@@ -30,6 +34,7 @@ function buildTable() {
  
 	var tableQuery = "SELECT * FROM products";
     
+    //goes through the database to populate table
     connection.query(tableQuery, function(err, res) {
       for (var i = 0; i < res.length; i++) {
       	table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
@@ -41,12 +46,13 @@ function buildTable() {
       
 	});
 
-	runBamazon();
+	runBamazon();//once table is built ask question to user
 
 };
 	
  
-
+//function that prompts user for what they what and how much
+//also does math to recalibrate the database
 function runBamazon(){
 
 
@@ -66,22 +72,25 @@ function runBamazon(){
 	]).then(function(user){
 		var query = "SELECT * FROM products WHERE ?";
 		var id = parseInt(user.idChoice);
+		//connect to database to so it can be updated if the quantity requested by user is available
 		connection.query(query, {item_id:id}, function(err, res){
 		
 			if(res[0].stock_quantity > user.quantity){
+				//subtracts the database quantity from what user is buying
 				var newQuantity = res[0].stock_quantity - user.quantity;
+				//calculates the total price
 				var totalPrice = user.quantity * res[0].price;
-
+				//updates the database to new quantity, kinda at the moment
 				connection.query("UPDATE products SET ? WHERE ??", [stock_quantity: newQuantity, item_id:id], function(err, res){
 					if(err) throw err;
 				});
-
+				//displays to user what and how much they bought as well as the total price
 				console.log("====================================");
 				console.log("You purchased " + user.quantity + " " + res[0].product_name);	
 				console.log("Your total price is: " + "$" + totalPrice);
 				console.log("====================================");
 				continueShopping();
-			}else{
+			}else{//if quantity is not available they are told so and asked to try again
 				console.log("Insufficient quantity available. Sorry!");
 				continueShopping();
 			};
@@ -95,6 +104,7 @@ function runBamazon(){
 
 };//end runBamazon function
 
+//function that asks the user if they would like another round of shopping
 function continueShopping (){
 	inquirer.prompt([
 		{
@@ -106,10 +116,15 @@ function continueShopping (){
 
 	]).then(function(user){
 		if(user.yesOrNo === "y"){
+			//if yes, starts all over
 			buildTable();
 		}else{
+			//if no we push them out the door
 			console.log("GET OUTTA HERE!!!");
 		}
 
 	});
 }
+
+//============
+//end of reuseable functions
